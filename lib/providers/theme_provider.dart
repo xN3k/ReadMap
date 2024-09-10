416c93728c8c71b02322c829c7c66ae326313dbe
xN3k <adarshchaudhary463@gmail.com>
2024-09-11 02:08:11 +0545

    added Shared_preferences to save themeData locally

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider with ChangeNotifier {
+  static const String _isDarkKey = 'isDark';
+
   bool _isDark = false;
   bool get isDark => _isDark;
 
-  void toggleTheme() {
+  ThemeProvider() {
+    _loadThemePreference();
+  }
+
+  Future<void> _loadThemePreference() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDark = prefs.getBool(_isDarkKey) ?? false;
+    notifyListeners();
+  }
+
+  Future<void> toggleTheme() async {
     _isDark = !_isDark;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_isDarkKey, isDark);
     notifyListeners();
   }
 
