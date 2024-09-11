713eb6e87ac55a1742bca8ec7294b74f46657ee3
xN3k <aadarshchaudhary463@gmail.com>
2024-09-11 23:22:54 +0545

    Created BookListSection

--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -1,4 +1,5 @@
 import 'package:book_app/screens/book_detail.dart';
+import 'package:book_app/widgets/book_list_section.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -21,35 +22,7 @@ class FavoriteScreen extends StatelessWidget {
               itemCount: favoriteBooks.length,
               itemBuilder: (context, index) {
                 final book = favoriteBooks[index];
-                return Card(
-                  child: ListTile(
-                    leading: book.imageLinks['thumbnail'] != null
-                        ? Image.network(
-                            book.imageLinks['thumbnail']!,
-                            fit: BoxFit.cover,
-                            width: 50,
-                            height: 50,
-                          )
-                        : Container(
-                            width: 50,
-                            height: 50,
-                            color: Colors.grey[200],
-                            child: Icon(Icons.book, color: Colors.grey[800]),
-                          ),
-                    title: Text(book.title),
-                    subtitle: Text('By ${book.authors.join(', ')}'),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => BookDetail(
-                            book: book,
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                );
+                return BookListSection(book: book);
               }),
     );
   }
--- lib/widgets/search_widget.dart
+++ lib/widgets/search_widget.dart
@@ -1,9 +1,9 @@
+import 'package:book_app/widgets/book_list_section.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../models/book.dart';
 import '../providers/book_provider.dart';
-import '../screens/book_detail.dart';
 
 class SearchSection extends StatefulWidget {
   const SearchSection({super.key});
@@ -38,37 +38,7 @@ class _SearchSectionState extends State<SearchSection> {
                         itemCount: bookProvider.books.length,
                         itemBuilder: (context, index) {
                           Book book = bookProvider.books[index];
-                          return Card(
-                            child: ListTile(
-                              leading: book.imageLinks['thumbnail'] != null
-                                  ? Image.network(
-                                      bookProvider.books[index]
-                                          .imageLinks['thumbnail']!,
-                                      fit: BoxFit.cover,
-                                      width: 50,
-                                      height: 50,
-                                    )
-                                  : Container(
-                                      width: 50,
-                                      height: 50,
-                                      color: Colors.grey[200],
-                                      child: Icon(Icons.book,
-                                          color: Colors.grey[800]),
-                                    ),
-                              title: Text(book.title),
-                              subtitle: Text('By ${book.authors.join(', ')}'),
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => BookDetail(
-                                      book: book,
-                                    ),
-                                  ),
-                                );
-                              },
-                            ),
-                          );
+                          return BookListSection(book: book);
                         },
                       ),
           ),
